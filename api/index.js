b406440a91ae696cda78c7b10eea06b1c335760e
Mohamad Tarhini <mohamed_tarhini@live.com>
2023-06-01 19:09:54 +0300

    update

--- api/index.js
+++ api/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
 const cors=require('cors');
+const mongoose=require('mongoose');
+const bcrypt = require('bcryptjs');// hash the password
+const UserModel = require('./models/User');
+
+var salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
+
 app.use(express.json());
 
-app.post('/register',(req,res)=>{
+mongoose.connect('mongodb+srv://mohamedtarhini95:mohamed123@cluster1.boziiha.mongodb.net/?retryWrites=true&w=majority')
+
+app.post('/register', async(req,res)=>{
     const{username,password}=req.body;
-    res.json({requestData:{username,password}});
+    try {
+        const userDoc = await UserModel.create({
+            username,
+            password:bcrypt.hashSync(password,salt)
+        });
+        res.json(userDoc);
+    } catch (error) {
+        res.status(400).json(error);
+    }
 });
 app.listen(4000)
-//mongodb+srv://mohamedtarhini95:mohamed123@cluster1.boziiha.mongodb.net/?retryWrites=true&w=majority
--- client/src/component/RegisterPage.js
+++ client/src/component/RegisterPage.js
@@ -1,15 +1,22 @@
 import { useState } from "react"
 
 export default function RegisterPage(){
+
     const [username,setUsername]=useState('');
     const [password,setPassword]=useState('');
+    
     async function register(e){
         e.preventDefault();
-        await fetch('http://localhost:4000/register',{
+        const response = await fetch('http://localhost:4000/register',{
             method:'POST',
             body:JSON.stringify({username,password}),
             headers:{'Content-Type':'application/json'},
-        })
+        });
+        if(response.status===200){
+            alert('Registration successful');
+        }else{
+            alert('Resgistration failed');
+        }
     }
     return(
         <form className="register" onSubmit={register}>
