c18df1ac37471a2d677b9eabce36a99b8f048d23
Mohamad Tarhini <mohamed_tarhini@live.com>
2023-06-01 16:16:50 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,20 @@ import './App.css';
 
 function App() {
   return (
-    <div >test</div>
+    <main>
+      <header>
+        <a href='' className='logo'></a>
+        <nav>
+          <a href=''>login</a>
+          <a href=''>Register</a>
+        </nav>
+      </header>
+        <div className='post'>
+          <img src='https://tse3.mm.bing.net/th?id=OIP.iSu2RcCcdm78xbxNDJMJSgHaEo&pid=Api&P=0&h=180' alt=''/>
+          <h2>Diddy is suing the co-owner of his tequila for racial discrimination </h2>
+          <p>In a lawsuit, filed Wednesday, Combs said that Diageo “kneecapped DeLeon’s sales growth for nearly a decade” because the company considered it a “Black” brand and marketed it to only “urban customers.” The suit also alleges that Diageo also neglected marketing for Ciroc, Diddy’s vodka brand he also co-owns with the company</p>
+        </div>
+    </main>
   );
 }
 
