ae1064cee2f6c6b3b983a845c3f30359416fd66b
Mohamad Tarhini <mohamed_tarhini@live.com>
2023-06-01 19:30:25 +0300

    update

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const cors=require('cors');
 const mongoose=require('mongoose');
 const bcrypt = require('bcryptjs');// hash the password
 const UserModel = require('./models/User');
+const jwt = require('jsonwebtoken');
 
 var salt = bcrypt.genSaltSync(10);
 
@@ -24,5 +25,20 @@ app.post('/register', async(req,res)=>{
     } catch (error) {
         res.status(400).json(error);
     }
+});
+
+app.post('/login', async(req,res)=>{
+    const{username,password}=req.body;
+        const userDoc=await UserModel.findOne({username});
+        const passOK = bcrypt.compareSync(password, userDoc.password);
+        if (passOK){
+            //logged in
+            jwt.sign({
+                username, id:userDoc._id
+            })
+        }else{
+        res.status(400).json("failed login");
+            
+        }
 });
 app.listen(4000)
