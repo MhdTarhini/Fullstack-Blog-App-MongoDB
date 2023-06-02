4b6e9dd3ddcfd7f2f0916ce22f6b6baaf7240ef6
Mohamad Tarhini <mohamed_tarhini@live.com>
2023-06-02 12:32:22 +0300

    update

--- client/src/component/CreatePostPage.js
+++ client/src/component/CreatePostPage.js
@@ -1,10 +1,41 @@
+import { useState } from "react"
+import ReactQuill from "react-quill"
+import 'react-quill/dist/quill.snow.css'
+
+const modules = {
+    toolbar: [
+        [{ 'header': [1, 2, false] }],
+        ['bold', 'italic', 'underline','strike', 'blockquote'],
+        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+        ['link', 'image'],
+        ['clean']
+    ],
+}
+const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+]
 export default function CreatePostPage (){
+    const [title,setTitle]=useState('')
+    const [summary,setSummary]=useState('')
+    const [content,setContent]=useState('')
+
     return(
         <form>
-            <input type="title" placeholder="Title"/>
-            <input type="summary" placeholder="Summary"/>
+            <input type="title" placeholder="Title" 
+            value={title} 
+            onChange={(e)=>{setTitle(e.target.value)}}/>
+            <input type="summary" placeholder="Summary" 
+            value={summary}
+            onChange={(e)=>{setSummary(e.target.value)}}/>
             <input type="file"/>
-            <textarea name="" id="" cols={30} rows={10}></textarea>
+            <ReactQuill 
+            value={content} 
+            onChange={setContent}
+            modules={modules} formats={formats}/>
+            <button style={{marginTop:'5px'}}>Create Post</button>
         </form>
     )
 }
